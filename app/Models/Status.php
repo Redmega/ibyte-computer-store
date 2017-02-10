f12ff36e218142983426719a2e4f13c275c43099
Angel Piscola <angelpiscola@gmail.com>
2017-02-10 01:57:59 -0500

    Create migration and model for User, Status tables

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -7,15 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 final class Status extends Model
 {
     public $timestamps = false;
-
-
+    
     /**
     * Get the orders which have this Status
     * Used in the Admin panel to look at orders.
     * TODO: Make Order model
     * @return Order[]
     */
+    /*
     public function orders() {
         return $this->belongsTo('App\Models\Order');
     }
+    */
 }
